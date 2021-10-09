d14703b085bbcf066bdb28529b1cb81cb4d2d18d
dante01yoon <bunggl@naver.com>
2021-10-09 11:32:19 +0900

    Update: socketIo 연결

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import * as http from "http";
+import * as express from "express";
+import * as socketIo from "socket.io";
+
+class App implements AppInterface{
+  application: express.Application;
+  server?: http.Server;
+  port: number;
+  io?: socketIo.Server;
+
+  constructor(port = 3001) {
+    this.application = express(); 
+    this.port = port; 
+  }
+
+  listen(port: number,callback?: () => void) {
+    this.server = this.application.listen(
+      port,
+      callback
+    );
+
+    this.io = new socketIo.Server(this.server);
+  }
+  
+}
+
+interface AppInterface{
+  application: express.Application;
+  port: number;
+  listen: (port: number, callback?: () => void) => void;
+}
+
+const app = new App().application;
+
+app.listen(3001,() => {
+  console.log("server running on port 3001");
+})
